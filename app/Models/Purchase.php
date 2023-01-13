2591eb685fa07c717a6ad6ff8c03104e21a22314
CoasterFreakDE <mleaked@gmail.com>
2023-01-13 13:14:34 +0100

    Tables and migration

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Purchase extends Model
-{
-    use HasFactory;
+class Purchase extends Model {
+
+    protected $table = 'purchases';
+
+    protected $fillable = [
+        'username',
+        'sellix_id',
+        'status',
+    ];
+
+
 }
--- database/migrations/2023_01_13_094346_create_purchases_table.php
+++ database/migrations/2023_01_13_094346_create_purchases_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('sellix_id');
+            $table->string('status');
             $table->timestamps();
         });
     }
