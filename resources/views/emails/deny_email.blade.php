77cf4234413486e51b083fceef47a0ee14ff039a
Thore Müller <75487485+LUPLUV@users.noreply.github.com>
2023-01-17 13:17:26 +0100

    Added Mail Notfiy
    + Approve Mail Template
    + Deny Mail Template
    + Applied mail Template

---
+++ resources/views/emails/deny_email.blade.php
@@ -0,0 +1,21 @@
+<p>
+    Hallo {{ $name }},
+    <br>
+    <br>
+    für deine Bewerbung und deinem darin zum Ausdruck gebrachten Interesse an einer Mitarbeit bei unserem Minecraft Server bedanken wir uns an dieser Stelle nochmal recht herzlich.
+    Wir schätzen die Mühe sehr, die du dir damit gemacht hast und dein Interesse an einer Mitarbeit in unserem Hause.
+    <br>
+    <br>
+    Nach sorgfältiger Prüfung deiner Bewerbungsunterlagen müssen wir dir bedauerlicherweise mitteilen, dass wir dich im weiteren Auswahlprozess nicht berücksichtigen können.
+    Die Tatsache, dass wir uns nicht für dich entschieden haben, bedeutet kein Werturteil über deine fachlichen oder persönlichen Qualifikationen.
+    Bitte habe dafür Verständnis, dass aufgrund anderer qualifizierter Bewerbungen oft nur Details entscheidend sind.
+    <br>
+    <br>
+    Wir hoffen, dass du uns trotz dieser enttäuschenden Nachricht verbunden bleibst und wünschen dir weiterhin alles Gute und viel Erfolg.
+    <br>
+    <br>
+    Viele Grüße
+    <br>
+    <br>
+    Dein SkyRealm Team
+</p>
