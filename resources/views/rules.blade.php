d646f3b9325e4f38f60d65bb1c20fb7c1d0634c8
CoasterFreakDE <mleaked@gmail.com>
2023-01-21 15:19:48 +0100

    Regelwerk hinzugefügt

--- resources/views/rules.blade.php
+++ resources/views/rules.blade.php
@@ -3,10 +3,113 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <img src="{{ asset('assets/rules.png') }}" alt="Banner Image" class="w-full rounded-lg">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="font-sans">{{ __("Willkommen auf SkyRealmDE!") }}</h1>
+                <div class="p-6 text-gray-900 dark:text-white">
+                    <div class="p-4">
+                        <div class="py-4">
+                            <h1 class="text-2xl underline font-black">§1 Verhaltenskodex</h1>
+                            <div class="px-4">
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.1 Unangemessene Sprache</h2>
+                                    <p class="text-sm">Jegliche Art von unangemessenen Sprache gegenüber anderen Spielern, dem Server oder anderen Beteiligten ist untersagt. Jeder hat das Recht auf eine respektvolle und höfliche Behandlung.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.2 Spamming & Schreibstil</h2>
+                                    <p class="text-sm">Wiederholung von Worten oder ähnlichen Ausdrücken innerhalb eines kurzen Zeitraums ist verboten. Werbung oder große Handelsanfragen sollten im Forum gepostet werden. Das Schreiben in Großbuchstaben im Chat ist ebenfalls untersagt.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.3 Inhalte</h2>
+                                    <p class="text-sm">Radikale, nationalsozialistische oder sexuelle Inhalte sowie jugendgefährdende Äußerungen sind nicht gestattet und können gegebenenfalls rechtliche Konsequenzen nach sich ziehen.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.4 Werbung</h2>
+                                    <p class="text-sm">Jegliche Art von Fremdwerbung innerhalb und außerhalb des Projekts "SkyRealm" ist untersagt.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.5 Drohungen & Provokationen</h2>
+                                    <p class="text-sm">Drohungen jeglicher Art sind verboten. Provokationen im öffentlichen Chat können ebenfalls bestraft werden.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h2 class="text-lg font-medium">§1.6 Zerstörungen</h2>
+                                    <p class="text-sm">Griefing (z.B. das Setzen von Lava oder Wasser auf fremdem Eigentum, das Zerstören von fremden Bauwerken, Zubauen von Hauseingängen, Verunstaltung der Landschaft, jegliche Art von Diebstahl oder das Töten von fremden Tieren) ist auf unserem Server untersagt.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-t border-gray-400 py-4">
+                            <h2 class="text-2xl underline font-black">§2 Bauregeln und Farming</h2>
+                            <div class="px-4">
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§2.1 Verbotene Bauwerke</h3>
+                                    <p class="text-sm">Verboten sind u.a. Bauwerke mit rassistischem, nationalistischem, sexuellem oder beleidigendem Hintergrund, Dauerhaft laufende Redstone Schaltungen, Anlagen, die explizit dazu dienen, den Server zu schädigen, Farmen, die nicht abschaltbar sind und auch ohne Anwesenheit des Besitzers laufen, Kakteenfarmen mit mehr als 1.000 Kakteen.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§2.2 Farmen & Tierhaltung</h3>
+                                    <p class="text-sm">Tiere sollten ausreichend Platz haben (maximal 1 Tier pro Block, ausgenommen Mobfarmen). Farmen, die der Performance des Servers schaden könnten (z.B. One-Tick-Farmen) sind verboten.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-t border-gray-400 py-4">
+                            <h2 class="text-2xl underline font-black">§3 Strafen</h2>
+                            <div class="px-4">
+                                <p class="text-sm py-2">Verstöße gegen diese Regeln können je nach Schwere des Vergehens zu Verwarnungen, temporären oder dauerhaften Banns führen. Jeder Fall wird individuell geprüft und die Strafe entsprechend festgelegt. Wiederholte Verstöße können zu härteren Strafen führen. Wir bitten um Verständnis, dass wir uns das Recht vorbehalten, ohne Angabe von Gründen jederzeit Strafen auszusprechen oder Regeln zu ändern.</p>
+                            </div>
+                        </div>
+                        <div class="border-t border-gray-400 py-4">
+                            <h2 class="text-2xl underline font-black">§4 Modifikationen des Clients</h2>
+                            <div class="px-4">
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§4.1 Grundsatz</h3>
+                                    <p class="text-sm">Veränderungen am Client, die dem Benutzer einen Vorteil gegenüber anderen Spielern verschaffen, sind grundsätzlich verboten, da sie die Fairness gegenüber anderen Spielern beeinträchtigen können. Beispiele für unerlaubte Modifikationen sind X-Ray, gehackte Clients, Autoklicker und Ähnliches.
 
-                    <p>Das Regelwerk wird aktuell noch erstellt.</p>
+                                        Rein clientseitige Mods, die das Spielerlebnis verbessern, sind hingegen ausdrücklich erlaubt. Hierzu zählen z.B. Mods wie Essential, JIT, diverse Mini/Worldmap Mods und Ähnliches.
+
+                                        Grundsätzlich gilt: Wenn du dir bei einem Mod nicht sicher bist, frage einfach im Discord nach</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-t border-gray-400 py-4">
+                            <h2 class="text-2xl underline font-black">§5 Handel</h2>
+                            <div class="px-4">
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§5.1 Preisregulierung</h3>
+                                    <p class="text-sm">Wir regulieren den Handel, Preise oder das Warenangebot nicht, abgesehen von den Spielbelohnungen. Dies ist die Verantwortung der Spieler.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§5.2 Betrug</h3>
+                                    <p class="text-sm">Absichtliches Betrügen beim Handeln ist nicht gestattet und wird bestraft. Rückerstattungen werden vorgenommen, wenn ein Spieler beim Handeln betrogen wurde. Beweise sollten in Form von Screenshots(F2) oder in Form eines Clips vorgelegt werden.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="border-t border-gray-400 py-4">
+                            <h2 class="text-2xl underline font-black">§6 Weitere Reglungen</h2>
+                            <div class="px-4">
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§6.1 Spielernamen</h3>
+                                    <p class="text-sm">Die Verwendung von irreführenden, anstößigen oder beleidigenden Namen ist verboten und wird bestraft.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§6.2 Bugs</h3>
+                                    <p class="text-sm">Jegliche Form von Bugs und deren Ausnutzung sind verboten. Wenn man einen Bug entdeckt, sollte man dies dem Team melden.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§6.3 Minecraft-Skins</h3>
+                                    <p class="text-sm">Moralisch verwerfliche und anstößige Minecraft-Skins, die gegen den Verhaltenskodex verstoßen, sind verboten.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§6.4 Umgehung von Strafen</h3>
+                                    <p class="text-sm">Wer versucht, eine bereits bestehende Strafe mit einem zweiten Account zu umgehen, wird härter bestraft und der zweite Account wird ebenfalls bestraft.</p>
+                                </div>
+                                <div class="py-2">
+                                    <h3 class="text-base font-medium">§6.5 Account Verantwortlichkeit</h3>
+                                    <p class="text-sm">Jeder ist für seinen eigenen Account verantwortlich. Daher wird eine Strafe auch dann ausgesprochen, wenn eine zweite Person außer dem Eigentümer einen Regelverstoß begeht.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="py-12 flex">
+                        <div class="flex-row justify-center">
+                            <p class="font-sans text-sm text-center">Stand: 21.01.2023 ◆ <small>Änderungen vorbehalten</small></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
