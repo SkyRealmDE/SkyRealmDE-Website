a2b5ed5af4330cbad7c5f4b3efae399b5358d21e
CoasterFreakDE <mleaked@gmail.com>
2023-02-19 21:34:40 +0100

    Changelog picture
    closes #37

--- resources/views/changelogs/index.blade.php
+++ resources/views/changelogs/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <img src="{{ asset('assets/jobs.png') }}" alt="Banner Image" class="w-full rounded-lg">
+            <img src="{{ asset('assets/changelogs.png') }}" alt="Banner Image" class="w-full rounded-lg">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-white">
                     <h1 class="font-sans text-4xl">Changelogs</h1>
