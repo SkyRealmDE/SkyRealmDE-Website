3f83766caede9169e7f5c94125e49920991907b1
Xanax <moritz.fuessel@flawcra.cc>
2023-04-16 03:12:38 +0200

    fix(skyrealm.php): correct typo in 'Data-Safety' key name

--- lang/en/skyrealm.php
+++ lang/en/skyrealm.php
@@ -6,15 +6,10 @@ return [
     |--------------------------------------------------------------------------
     | SkyRealm Language Lines
     |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
     */
 
     'imprint' => 'Imprint',
     'tos' => 'TOS',
-    'data-safety' => 'Data-Saftey',
+    'data-safety' => 'Data-Safety',
 
 ];
